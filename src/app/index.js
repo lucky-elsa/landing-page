a813febdaa8efb5d6b03ffab0bce4dabd2c7bd9b
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-01-29 00:57:36 +0000

    add functionality to burger nav

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,14 @@
 import("../styles/sass/main.scss");
 import html from "../index.html";
 
-console.log("hello world");
+const menuButtonOpen = document.querySelector(".s-header__burger");
+const menuButtonClose = document.querySelector(".menu__close-burger");
+const menu = document.querySelector(".s-header__menu");
+
+menuButtonOpen.addEventListener("click", function () {
+    menu.classList.remove("hidden");
+});
+
+menuButtonClose.addEventListener("click", function () {
+    menu.classList.add("hidden");
+});
